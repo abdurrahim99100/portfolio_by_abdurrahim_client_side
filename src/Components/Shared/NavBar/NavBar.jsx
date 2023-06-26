01be6db0726b9f9d81d2d4ae257d0215e381d2e2
cornollc <abdurrahim99100@gmail.com>
2023-06-26 23:38:51 +0600

    navBar okay

--- src/Components/Shared/NavBar/NavBar.jsx
+++ src/Components/Shared/NavBar/NavBar.jsx
@@ -1,8 +1,49 @@
-
+import { Link } from 'react-router-dom';
+import profileImg from '../../../assets/profilePicture/20190119_133806~2.jpg';
 const NavBar = () => {
+    const users = false;
+    const navItems =
+        <>
+            <li className='hover:bg-gray-300 rounded'> <Link>home</Link> </li>
+            <li className='hover:bg-gray-300 rounded'> <Link>about</Link> </li>
+            <li className='hover:bg-gray-300 rounded'> <Link>skills</Link> </li>
+            <li className='hover:bg-gray-300 rounded'> <Link>project</Link> </li>
+            <li className='hover:bg-gray-300 rounded'> <Link>comments</Link> </li>
+            {
+                users ?
+                    <>
+                        <li>logout</li>
+                    </> :
+                    <>
+                        <li>login</li>
+                    </>
+            }
+        </>
     return (
-        <div>
-            <h3>This is nav bar</h3>
+        <div className="navbar justify-between">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 text-black uppercase items-start">
+                        {
+                            navItems
+                        }
+                    </ul>
+                </div>
+                <button className='flex items-center gap-2'>
+                    <img className='w-10 h-10 rounded-full' src={profileImg} alt="" />
+                    <p className='text-2xl font-semibold'>Abdur Rahim</p>
+                </button>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1 uppercase font-semibold items-center">
+                    {
+                        navItems
+                    }
+                </ul>
+            </div>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import router from './Router/Router.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div className='max-w-screen-2xl mx-auto'>
+    <div className='max-w-screen-2xl mx-auto bg-[#0C090A] text-white'>
       <RouterProvider router={router}></RouterProvider>
     </div>
   </React.StrictMode>,
